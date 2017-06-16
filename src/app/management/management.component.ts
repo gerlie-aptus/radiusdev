6a60c9afeaa7db96636e23f0f337be285ba3d2d8
Gerlie Reyes <gerlie.reyes@aptus.ph>
2017-06-16 14:41:54 +0800

    Form Validation done

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -3,7 +3,7 @@ import { ManagementService } from '../services/management.service';
 // import {environment} from "../../environments/environment";
 import { Router } from '@angular/router';
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -16,28 +16,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ManagementComponent implements OnInit {
     managementForm : FormGroup;
 
+    wallGardenControl = new FormControl('');
+    shapeControl = new FormControl('');
+
 
     constructor(private router: Router, private managementService : ManagementService, private  fb: FormBuilder) {
         this.createForm();
     }
 
     ngOnInit() {
-
     }
 
     createForm(){
         this.managementForm = this.fb.group({
-            username: ['', Validators.required ],
-            password: '',
-            confirmPassword: '',
-            redirection: '',
-            static_ip: ''
+            username: ['', Validators.pattern('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}')],
+            // redirection: ['', Validators.pattern('(https?:\/\/)(?!www | www\.)+([\da-z\.-]+)\.([a-z\.]{2,6})+([\/\w \.-])?(.*)')],
+            redirection: ['', Validators.pattern('\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]')],
+            static_ip: ['', Validators.pattern('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')],
+            passwords: this.fb.group({
+                password: ['', Validators.minLength(4)],
+                confirmPassword: ['', Validators.minLength(4)],
+            }, {validator: this.passwordMatchValidator})
         });
+    }
+
+    passwordMatchValidator(g: FormGroup) {
+        return g.get('password').value === g.get('confirmPassword').value
+            ? null : {'mismatch': true};
     }
 
     submitForm(value: any):void{
         console.log('Reactive Form Data: ')
         console.log(value);
+        console.log(value.passwords);
     }
 
     ping(){
